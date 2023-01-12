a40b65eafb6b6a3fa0a9d9880eaaff1d66442411
ibra <ibrahimalanshor6@gmail.com>
2023-01-12 15:49:42 +0800

    feat: alert test

---
+++ src/tests/bangui-alert.test.js
@@ -0,0 +1,139 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest';
+import { h, ref } from 'vue';
+import { BanguiAlert } from '../components';
+import alertClass from '../helpers/alert-class.js';
+
+describe('alert test', () => {
+  test('component exists', async () => {
+    expect(BanguiAlert).toBeDefined();
+  });
+
+  test('default', async () => {
+    const wrapper = mount(BanguiAlert);
+
+    expect(wrapper.find(`.${alertClass.wrapper}`).exists()).toBeFalsy();
+  });
+
+  test('visible', async () => {
+    const visible = ref(true);
+
+    const wrapper = mount(BanguiAlert, {
+      props: {
+        modelValue: visible.value,
+      },
+    });
+
+    expect(wrapper.find(`.${alertClass.wrapper}`).exists()).toBeTruthy();
+  });
+
+  test('message from props', async () => {
+    const visible = ref(true);
+    const message = 'Test Message';
+
+    const wrapper = mount(BanguiAlert, {
+      props: {
+        modelValue: visible.value,
+        message,
+      },
+    });
+
+    expect(wrapper.text()).toEqual(message);
+  });
+
+  test('message from slot', async () => {
+    const visible = ref(true);
+    const message = 'Test Message';
+
+    const wrapper = mount(BanguiAlert, {
+      props: {
+        modelValue: visible.value,
+        message: 'Overrided',
+      },
+      slots: {
+        default: message,
+      },
+    });
+
+    expect(wrapper.text()).toEqual(message);
+  });
+
+  test('icon', async () => {
+    const visible = ref(true);
+
+    const wrapper = mount(BanguiAlert, {
+      props: {
+        modelValue: visible.value,
+      },
+      slots: {
+        icon: h('span', {}, 'Icon'),
+      },
+    });
+
+    expect(wrapper.find('span').exists()).toBeFalsy();
+
+    await wrapper.setProps({ withIcon: true });
+
+    expect(wrapper.find('span').exists()).toBeTruthy();
+  });
+
+  test('closable', async () => {
+    const visible = ref(true);
+
+    const wrapper = mount(BanguiAlert, {
+      props: {
+        modelValue: visible.value,
+      },
+      slots: {
+        close: ({ close }) => h('span', { onClick: close }, 'Close'),
+      },
+    });
+
+    expect(wrapper.find('span').exists()).toBeFalsy();
+
+    await wrapper.setProps({ closeable: true });
+
+    expect(wrapper.find('span').exists()).toBeTruthy();
+
+    await wrapper.find('span').trigger('click');
+
+    expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+    expect(wrapper.emitted()['update:modelValue'][0][0]).toBeFalsy();
+
+    visible.value = false;
+
+    await wrapper.setProps({ modelValue: visible.value });
+
+    expect(wrapper.find('div').exists()).toBeFalsy();
+
+    visible.value = true;
+
+    await wrapper.setProps({
+      modelValue: visible.value,
+      autoClose: true,
+      timeout: 1000,
+    });
+
+    expect(wrapper.find('div').exists()).toBeTruthy();
+
+    // Not Working : How To Use Set Timout in Test Vitest
+    setTimeout(() => {
+      expect(wrapper.find('div').exists()).toBeFalsy();
+    }, 1000);
+  });
+
+  test('color', async () => {
+    for (const colorName in alertClass.colors) {
+      const wrapper = mount(BanguiAlert, {
+        props: {
+          modelValue: true,
+          color: colorName,
+        },
+      });
+
+      expect(
+        wrapper.find(`.${alertClass.colors[colorName]}`).exists()
+      ).toBeTruthy();
+    }
+  });
+});
