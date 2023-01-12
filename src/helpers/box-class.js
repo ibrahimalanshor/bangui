35f038d75097a70b1beacb026fe2c9809eb9eabb
ibra <ibrahimalanshor6@gmail.com>
2023-01-12 17:52:15 +0800

    feat: box class

---
+++ src/helpers/box-class.js
@@ -0,0 +1,12 @@
+export default {
+  wrapper: '--bangui-box',
+  colors: {
+    primary: '--bangui-box-color-primary',
+    secondary: '--bangui-box-color-primary',
+    danger: '--bangui-box-color-danger',
+    success: '--bangui-box-color-success',
+    warning: '--bangui-box-color-warning',
+    light: '--bangui-box-color-light',
+    dark: '--bangui-box-color-light',
+  },
+};
