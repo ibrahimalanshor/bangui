22e42be89a65fd7add24d5dfe62c84c3778ffd7f
ibra <ibrahimalanshor6@gmail.com>
2023-01-12 17:52:01 +0800

    feat: box component

---
+++ src/components/bangui-box.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue';
+import boxClass from '../helpers/box-class';
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: 'light',
+  },
+});
+
+const style = computed(() => {
+  return [
+    boxClass.wrapper,
+    boxClass.colors[props.color] ?? boxClass.colors.default,
+  ];
+});
+</script>
+
+<template>
+  <div :class="style">
+    <slot />
+  </div>
+</template>
