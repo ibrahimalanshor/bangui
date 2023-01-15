6b49977d7b6aa29b4d74ec812f72f81bedd00c57
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-15 12:38:39 +0700

    feat: modal class

---
+++ src/helpers/modal-class.js
@@ -0,0 +1,13 @@
+export default {
+  wrapper: '--bangui-modal',
+  content: '--bangui-modal-content',
+  sizes: {
+    sm: '--bangui-modal-size-sm',
+    md: '--bangui-modal-size-md',
+    lg: '--bangui-modal-size-lg',
+  },
+  header: '--bangui-modal-header',
+  title: '--bangui-modal-title',
+  body: '--bangui-modal-body',
+  footer: '--bangui-modal-footer',
+};
