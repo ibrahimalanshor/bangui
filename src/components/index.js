8fb957c3fb3e076cd7f3bc1e59798d181d79a5be
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-15 12:38:09 +0700

    feat: modal component

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ export { default as BanguiCheckbox } from './bangui-checkbox.vue';
 export { default as BanguiDropdown } from './bangui-dropdown.vue';
 export { default as BanguiForm } from './bangui-form.vue';
 export { default as BanguiInput } from './bangui-input.vue';
+export { default as BanguiModal } from './bangui-modal.vue';
 export { default as BanguiRadio } from './bangui-radio.vue';
 export { default as BanguiSelect } from './bangui-select.vue';
 export { default as BanguiTextarea } from './bangui-textarea.vue';
