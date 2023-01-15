f98ad35c3f434bf3a9fa831d97fdfa965c170f57
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-15 10:31:20 +0700

    feat: dropdowmn class

---
+++ src/helpers/dropdown-class.js
@@ -0,0 +1,15 @@
+export default {
+  wrapper: '--bangui-dropdown',
+  content: '--bangui-dropdown-content',
+  item: '--bangui-dropdown-item',
+  sizes: {
+    sm: '--bangui-dropdown-size-sm',
+    md: '--bangui-dropdown-size-md',
+    lg: '--bangui-dropdown-size-lg',
+    fullwidth: '--bangui-dropdown-size-fullwidth',
+  },
+  positions: {
+    left: '--bangui-dropdown-position-left',
+    right: '--bangui-dropdown-position-right',
+  },
+};
