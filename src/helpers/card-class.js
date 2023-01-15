c91c567f4c78af73d1db0f9509f74c7f10a42b30
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-15 09:22:14 +0700

    feat: card class

---
+++ src/helpers/card-class.js
@@ -0,0 +1,7 @@
+export default {
+  wrapper: '--bangui-card',
+  header: '--bangui-card-header',
+  title: '--bangui-card-title',
+  body: '--bangui-card-body',
+  footer: '--bangui-card-footer',
+};
