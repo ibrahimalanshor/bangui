e7ec8f67f7aa3f94d6bc52885439edcdf99ed89b
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-16 10:32:22 +0700

    feat: pagination class

---
+++ src/helpers/pagination-class.js
@@ -0,0 +1,8 @@
+export default {
+  wrapper: '--bangui-pagination',
+  prev: '--bangui-pagination-prev',
+  next: '--bangui-pagination-next',
+  item: '--bangui-pagination-item',
+  disabled: '--bangui-pagination-disabled',
+  active: '--bangui-pagination-active',
+};
