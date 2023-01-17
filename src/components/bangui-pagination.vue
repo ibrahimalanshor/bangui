9d9966aa14dd680c67d28f6b7b7a5a59fd732d19
ibra <ibrahimalanshor6@gmail.com>
2023-01-17 13:07:03 +0800

    feat: pagination event emit

--- src/components/bangui-pagination.vue
+++ src/components/bangui-pagination.vue
@@ -15,11 +15,16 @@ const props = defineProps({
     type: Number,
     required: true,
   },
+  perPage: {
+    type: Number,
+    default: 10,
+  },
 });
 const emit = defineEmits(['page-change', 'prev', 'next']);
 
 const activePage = ref(1);
 
+const itemsLength = computed(() => Math.round(props.size / props.perPage));
 const style = computed(() => {
   return {
     wrapper: [paginationClass.wrapper],
@@ -32,12 +37,18 @@ const style = computed(() => {
 });
 
 function handleClickPrev() {
+  activePage.value--;
+
   emit('prev');
 }
 function handleClickNext() {
+  activePage.value++;
+
   emit('next');
 }
 function handleClickItem(pageNumber) {
+  activePage.value = pageNumber;
+
   emit('page-change', pageNumber);
 }
 </script>
@@ -45,21 +56,21 @@ function handleClickItem(pageNumber) {
 <template>
   <div :class="style.wrapper">
     <div
-      :class="[style.prev, activePage !== 1 ? style.disabled : '']"
+      :class="[style.prev, activePage === 1 ? style.disabled : '']"
       v-on:click="handleClickPrev"
     >
       <slot name="prev">{{ props.prevLabel }}</slot>
     </div>
     <div
-      v-for="item in size"
+      v-for="item in itemsLength"
       :key="item"
       :class="[style.item, activePage === item ? style.active : '']"
-      v-on:click="handleClickItem"
+      v-on:click="handleClickItem(item)"
     >
       {{ item }}
     </div>
     <div
-      :class="[style.next, activePage === props.size ? style.disabled : '']"
+      :class="[style.next, activePage === itemsLength ? style.disabled : '']"
       v-on:click="handleClickNext"
     >
       <slot name="next">{{ props.nextLabel }}</slot>
