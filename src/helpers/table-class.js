1a5d55d20ea37409de894fdf1508be0ea34b32d4
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-17 18:11:04 +0700

    feat: table class

---
+++ src/helpers/table-class.js
@@ -0,0 +1,10 @@
+export default {
+  table: '--bangui-table',
+  thead: '--bangui-table-thead',
+  tr: '--bangui-table-tr',
+  theadTr: '--bangui-table-thead-tr',
+  th: '--bangui-table-th',
+  tbody: '--bangui-table-tbody',
+  tbodyTr: '--bangui-table-tbody-tr',
+  td: '--bangui-table-td',
+};
