d336de0134670bc786c9ebdac41f82138bfd15eb
ibra <ibrahimalanshor6@gmail.com>
2023-01-18 09:29:19 +0800

    fix: classnames

--- src/helpers/box-class.js
+++ src/helpers/box-class.js
@@ -1,12 +1,12 @@
 export default {
-  wrapper: '--bangui-box',
+  wrapper: 'bangui-box',
   colors: {
-    primary: '--bangui-box-color-primary',
-    secondary: '--bangui-box-color-primary',
-    danger: '--bangui-box-color-danger',
-    success: '--bangui-box-color-success',
-    warning: '--bangui-box-color-warning',
-    light: '--bangui-box-color-light',
-    dark: '--bangui-box-color-light',
+    primary: 'bangui-box-color-primary',
+    secondary: 'bangui-box-color-primary',
+    danger: 'bangui-box-color-danger',
+    success: 'bangui-box-color-success',
+    warning: 'bangui-box-color-warning',
+    light: 'bangui-box-color-light',
+    dark: 'bangui-box-color-light',
   },
 };
--- src/helpers/modal-class.js
+++ src/helpers/modal-class.js
@@ -1,13 +1,13 @@
 export default {
-  wrapper: '--bangui-modal',
-  content: '--bangui-modal-content',
+  wrapper: 'bangui-modal',
+  content: 'bangui-modal-content',
   sizes: {
-    sm: '--bangui-modal-size-sm',
-    md: '--bangui-modal-size-md',
-    lg: '--bangui-modal-size-lg',
+    sm: 'bangui-modal-size-sm',
+    md: 'bangui-modal-size-md',
+    lg: 'bangui-modal-size-lg',
   },
-  header: '--bangui-modal-header',
-  title: '--bangui-modal-title',
-  body: '--bangui-modal-body',
-  footer: '--bangui-modal-footer',
+  header: 'bangui-modal-header',
+  title: 'bangui-modal-title',
+  body: 'bangui-modal-body',
+  footer: 'bangui-modal-footer',
 };
