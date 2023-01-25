0a747a2e66099708edc47a3c46820c895002d0e3
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2023-01-25 19:27:43 +0700

    spinner slot tesrt

--- src/tests/bangui-loading.test.js
+++ src/tests/bangui-loading.test.js
@@ -14,4 +14,16 @@ describe('loading test', () => {
     expect(wrapper.find(`.${loadingClass.wrapper}`).exists()).toBeTruthy();
     expect(wrapper.find(`.${loadingClass.spinner}`).exists()).toBeTruthy();
   });
+
+  test('spinner slot', () => {
+    const wrapper = mount(BanguiLoading, {
+      slots: {
+        default: 'Custom Slot',
+      },
+    });
+
+    expect(wrapper.find(`.${loadingClass.wrapper}`).text()).toEqual(
+      'Custom Slot'
+    );
+  });
 });
